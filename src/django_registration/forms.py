9ac6a282bc20cc0ff816d86d3ad73fb4c84619d4
Kevin Barnes <kbarnes3@gmail.com>
2022-07-15 17:17:46 -0700

    Sorting imports

--- src/django_registration/forms.py
+++ src/django_registration/forms.py
@@ -10,8 +10,8 @@ django-registration.
 """
 
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 from . import validators
