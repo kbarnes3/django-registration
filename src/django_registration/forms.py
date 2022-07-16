d770bcd0b57497aa740bbd0490478ba30e883469
Kevin Barnes <kbarnes3@gmail.com>
2022-07-15 17:14:19 -0700

    Hardcoding forms to use Django's default User model

--- src/django_registration/forms.py
+++ src/django_registration/forms.py
@@ -10,13 +10,11 @@ django-registration.
 """
 
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
 
 from . import validators
-
-User = get_user_model()
 
 
 class RegistrationForm(UserCreationForm):
